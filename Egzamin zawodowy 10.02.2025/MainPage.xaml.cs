9c24b457799dcb94507b4800f34ae0efd6e5285f
Kacper <113052358+KacperShaggy@users.noreply.github.com>
2025-02-17 11:22:12 +0100

    Add files via upload

---
+++ Egzamin zawodowy 10.02.2025/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace DomekWGorach
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLike_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            labelCounter.Text = $"{count} polubień";
+        }
+
+        private void buttonDislike_Clicked(object sender, EventArgs e)
+        {
+            if (count > 0)
+            {
+                count--;
+            }
+            labelCounter.Text = $"{count} polubień";
+        }
+    }
+
+}
