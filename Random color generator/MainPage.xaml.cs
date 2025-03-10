067fb6c62b676fe808c7dffb02e610dec7dbaaf4
Kacper <113052358+KacperShaggy@users.noreply.github.com>
2025-03-10 11:21:52 +0100

    Add files via upload

---
+++ Random color generator/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int random = new Random();
+
+        Color color = Color.FromRgb(
+
+                random.Next(0, 255),
+                random.Next(0, 255),
+                random.Next(0, 255)
+
+        );
+
+       
+
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+
+        private void SetColor(Color color)
+        {
+            randomButton.BackgroundColor = color;
+            Container.BackgroundColor = color;
+            labelHex.Text = color.ToHex();
+
+            
+        }
+        
+        private void RandomButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
