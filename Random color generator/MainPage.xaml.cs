67dac46c213cb92c4bbde28bc21fcbf195ffefa1
Kacper <113052358+KacperShaggy@users.noreply.github.com>
2025-03-17 11:19:52 +0100

    Add files via upload

--- Random color generator/MainPage.xaml.cs
+++ Random color generator/MainPage.xaml.cs
@@ -1,44 +1,53 @@
-﻿namespace MauiApp2
+﻿using System;
+using System.ComponentModel;
+
+namespace MauiApp2
+
 {
+
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        int random = new Random();
-
-        Color color = Color.FromRgb(
-
-                random.Next(0, 255),
-                random.Next(0, 255),
-                random.Next(0, 255)
-
-        );
-
-       
-
-
-        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        private void randomButtonClicked(object sender, EventArgs e)
         {
+            Random random = new Random();
 
+            int red = random.Next(0, 255);
+            int green = random.Next(0, 255);
+            int blue = random.Next(0, 255);
+
+            r_slide.Value = red;
+            g_slide.Value = green;
+            b_slide.Value = blue;
+        }
+
+        private void changeBG(object sender, ValueChangedEventArgs e)
+        {
+            int red = (int)r_slide.Value;
+            int green = (int)g_slide.Value;
+            int blue = (int)b_slide.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            hex.Text = Color.FromRgb(red, green, blue).ToHex();
+
+            Container.BackgroundColor = color;
+
+            hex.Text = color.ToHex();
         }
 
         private void SetColor(Color color)
         {
             randomButton.BackgroundColor = color;
-            Container.BackgroundColor = color;
-            labelHex.Text = color.ToHex();
 
-            
-        }
-        
-        private void RandomButton_Clicked(object sender, EventArgs e)
-        {
+            Container.BackgroundColor = color;
+
+            hex.Text = color.ToHex();
 
         }
     }
-
 }
